da47c6a3deb48e89bda09e584a1037d1eeb6a4d1
Marcos <marcospalo22@gmail.com>
2026-03-28 19:12:56 -0300

    Segundo  Commit proyecto

--- src/nuevo.hs
+++ src/nuevo.hs
@@ -1,1 +1,12 @@
-saludar nombre = "Hola "++ nombre :: [CHAR]
+import GHC.Base
+import PdePreludat (putStrLn, getLine)
+
+saludar :: String -> String
+saludar nombre  = "Hola " ++ nombre ++ "!"
+
+main :: IO ()
+main = do
+    putStrLn "Ingrese su nombre"
+    nombre <- getLine
+    let saludo = saludar nombre
+    putStrLn saludo 
